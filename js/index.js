ec1a11184cf8cb4eeb550c29c50b576b7139ade3
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2022-12-10 22:50:56 +0100

    Add Mode Responsive

--- js/index.js
+++ js/index.js
@@ -1,5 +1,18 @@
+// Modo Oscuro
 let buttonDark = document.querySelector(".enlace__icon");
-
 buttonDark.addEventListener('click', () => {
     document.body.classList.toggle("modo__oscuro")
+})
+
+// Menu Hamburger
+let menu = document.querySelector(".hamburger")
+let navegacion = document.querySelectorAll(".navegacion__enlaces .navegacion__enlace")
+menu.addEventListener('click', () => {
+    navegacion.forEach(item => {
+        if (item.style.display == "none") {
+            item.style.display = "unset"
+        } else {
+            item.style.display = "none"
+        }
+    })
 })
