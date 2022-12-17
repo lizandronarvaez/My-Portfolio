1ab3d670574acde0aead45fe43b809c7259ef66f
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2022-12-17 15:53:25 +0100

    Correction Response

--- js/index.js
+++ js/index.js
@@ -1,8 +1,3 @@
-// Modo Oscuro
-let buttonDark = document.querySelector(".enlace__icon");
-buttonDark.addEventListener('click', () => {
-    document.body.classList.toggle("modo__oscuro")
-})
 
 // Menu Hamburger
 let menu = document.querySelector(".hamburger")
@@ -10,3 +5,10 @@ let navegacion = document.querySelector(".navegacion__enlaces")
 menu.addEventListener('click', () => {
     navegacion.classList.toggle('mostrar__enlaces')
 })
+// Modo Oscuro
+let buttonDark = document.querySelector(".enlace__icon");
+let menuSvg=document.querySelector(".hamburger svg")
+buttonDark.addEventListener('click', () => {
+    document.body.classList.toggle("modo__oscuro")
+})
+
